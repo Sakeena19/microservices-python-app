5392e8b333aa33b42c1519f6a6b165e570ef3771
Sakeena19 <133941373+Sakeena19@users.noreply.github.com>
2024-04-01 22:56:07 +0530

    Update consumer.py

--- src/converter-service/consumer.py
+++ src/converter-service/consumer.py
@@ -7,43 +7,49 @@ import gridfs
 from convert import to_mp3
 
 def main():
-    client = MongoClient(os.environ.get('MONGODB_URI'))
-    db_videos = client.videos
-    db_mp3s = client.mp3s
-    # gridfs
-    fs_videos = gridfs.GridFS(db_videos)
-    fs_mp3s = gridfs.GridFS(db_mp3s)
-
-    # rabbitmq connection
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='rabbitmq', heartbeat=0)
-    )
-    channel = connection.channel()
-
-    # Declare the queue
-    channel.queue_declare(queue=os.environ.get("VIDEO_QUEUE"))
-
-    def callback(ch, method, properties, body):
-        err = to_mp3.start(body, fs_videos, fs_mp3s, ch)
-        if err:
-            ch.basic_nack(delivery_tag=method.delivery_tag)
-        else:
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-
-    channel.basic_consume(
-        queue=os.environ.get("VIDEO_QUEUE"), on_message_callback=callback
-    )
-
-    print("Waiting for messages, to exit press CTRL+C")
-
-    channel.start_consuming()
-
-if __name__ == "__main__":
     try:
-        main()
+        # Connect to MongoDB
+        client = MongoClient(os.environ.get('MONGODB_URI'))
+        db_videos = client.videos
+        db_mp3s = client.mp3s
+        # gridfs
+        fs_videos = gridfs.GridFS(db_videos)
+        fs_mp3s = gridfs.GridFS(db_mp3s)
+
+        # Connect to RabbitMQ
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='rabbitmq', heartbeat=0)
+        )
+        channel = connection.channel()
+
+        # Declare the queue
+        queue_name = os.environ.get("VIDEO_QUEUE")
+        channel.queue_declare(queue=queue_name)
+
+        def callback(ch, method, properties, body):
+            err = to_mp3.start(body, fs_videos, fs_mp3s, ch)
+            if err:
+                ch.basic_nack(delivery_tag=method.delivery_tag)
+            else:
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+
+        # Start consuming messages from the queue
+        channel.basic_consume(
+            queue=queue_name, on_message_callback=callback
+        )
+
+        print("Waiting for messages, to exit press CTRL+C")
+
+        # Start consuming messages
+        channel.start_consuming()
+
     except KeyboardInterrupt:
         print("Interrupted")
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+        sys.exit(0)
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
